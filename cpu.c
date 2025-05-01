445b22aa445d91fb540312a71da3886e8244600f
Magooch <crvallecito@gmail.com>
2025-04-30 19:23:08 -0700

    Initialize run_cpu function with test code

--- cpu.c
+++ cpu.c
@@ -1,6 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+char* run_cpu(char** instructions) {
+    for (int i = 0; i < 100; i++) {
+        if (instructions[i] == "END") {
+            printf("Program ended\n");
+            return 0;
+        } else {
+            printf("%s, ", instructions[i]);
+        }
+    }
+    // printf("%s", instructions[1]);
+    return 0;
+}
+
 int main() {
+    char* test_arr[] = {"LOAD x", "test complete", "test", "END"};
+    run_cpu(test_arr);
     return 0;
 }
